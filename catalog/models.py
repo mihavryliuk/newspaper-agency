e163aefc49ff5100909df39a40496cd3b5a70ffd
Misha Havryliuk <misharob31415@gmail.com>
2024-09-22 15:16:14 +0300

    started fixing look

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class Topic(models.Model):
@@ -14,6 +15,9 @@ class Redactor(AbstractUser):
 
     def __str__(self):
         return self.username
+
+    def get_absolute_url(self):
+        return reverse('catalog:redactor-detail', args=[str(self.id)])
 
 
 class Newspaper(models.Model):
