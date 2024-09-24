afc3802420bbb54ac51ff1232a0b4b8b00a81b9a
Misha Havryliuk <misharob31415@gmail.com>
2024-09-24 14:36:12 +0300

    migrate

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -13,12 +13,16 @@ import os
 import dj_database_url
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
