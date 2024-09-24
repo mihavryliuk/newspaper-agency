a87c638e2783600a0d87c2aeae47a496f01559d3
Misha Havryliuk <misharob31415@gmail.com>
2024-09-24 14:39:59 +0300

    ALLOWED_HOSTS

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "newspaper-agency-96fn.onrender.com/"]
 
 
 # Application definition
